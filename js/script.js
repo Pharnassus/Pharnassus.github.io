c9679b7529bea2af033bfc21dbea2bfb0d52b636
Marian <marian.corlade@outlook.it>
2021-04-22 00:04:53 +0200

    end nav responsive

--- js/script.js
+++ js/script.js
@@ -32,9 +32,18 @@ haburger icon
 */
 function myFunction() {
     var x = document.getElementById("myLinks");
+    var y = document.getElementById("hamburger_nav_ms");
     if (x.style.display === "block") {
         x.style.display = "none";
+        y.style.position = "absolute";
+        y.style.top = "22px";
+        y.style.left = "375px";
+        y.style.fontSize = "30px";
     } else {
         x.style.display = "block";
+        y.style.position = "relative";
+        y.style.top = "20px";
+        y.style.left = "60px";
+        y.style.fontSize = "30px";
     }
 }
