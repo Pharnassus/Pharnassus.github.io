8ba4bc89a0566ab98db03ab5f1c2a58c5dffbcca
Marian <marian.corlade@outlook.it>
2021-04-21 21:51:55 +0200

    light/dark nav

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-// When the user scrolls down 80px from the top of the document,
-// resize the navbar's padding and the logo's font size
-window.onscroll = function () { scrollFunction() };
+//Dark/Light Mode
+var themeModeCheck = document.querySelector('input[name=theme]');
 
-function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("navbar").style.padding = "30px 10px";
-        document.getElementById("logo").style.fontSize = "25px";
+themeModeCheck.addEventListener('change', function () {
+    if (this.checked) {
+        trans();
+        document.documentElement.setAttribute('data-theme', 'dark');
+        document.cookie = "themeMode=dark";
     } else {
-        document.getElementById("navbar").style.padding = "80px 10px";
-        document.getElementById("logo").style.fontSize = "35px";
+        trans();
+        document.documentElement.setAttribute('data-theme', 'light');
+        document.cookie = "themeMode=light";
     }
+})
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
 }
